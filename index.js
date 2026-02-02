0e7250b238b8e1ffd68468a849ebd1b60db81211
matthatbhatti <matthatbhatti01@gmail.com>
2026-02-02 16:40:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+web: node index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static("build"));
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/project.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
